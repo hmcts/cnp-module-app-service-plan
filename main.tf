0666b1c6e9ef5e4341e51a00749f6a436c939b54
Ben <bhicks329@gmail.com>
2018-05-17 22:37:10 +0100

    ase_)name

--- main.tf
+++ main.tf
@@ -4,18 +4,18 @@ data "template_file" "deployASP" {
 
 # Deploy the App Service Plan
 resource "azurerm_template_deployment" "app_service_plan" {
-  template_body     = "${data.template_file.deployASP.rendered}"
-  name              = "${var.product}-${var.env}-asp-webapp"
-  resourcegroupname = "${var.resource_group_name}"
-  deployment_mode   = "Incremental"
+  template_body       = "${data.template_file.deployASP.rendered}"
+  name                = "${var.product}-${var.env}-asp-webapp"
+  resource_group_name = "${var.resource_group_name}"
+  deployment_mode     = "Incremental"
 
   parameters = {
-    name          = "${var.product}-${var.env}"
-    location      = "${var.location}"
-    env           = "${var.env}"
-    asp_capacity  = "${var.asp_capacity}"
-    asp_name      = "${var.asp_name}"
-    ase_name_list = "${var.ase_name_list}"
-    tag_list      = "${var.tag_list}"
+    name         = "${var.product}-${var.env}"
+    location     = "${var.location}"
+    env          = "${var.env}"
+    asp_capacity = "${var.asp_capacity}"
+    asp_name     = "${var.asp_name}"
+    ase_name     = "${var.ase_name_list}"
+    tag_list     = "${var.tag_list}"
   }
 }
