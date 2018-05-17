46edde5dba131ac6c81952afd73294f269998ee4
Ben <bhicks329@gmail.com>
2018-05-17 15:49:17 +0100

    updating variables

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ data "template_file" "deployASP" {
 resource "azurerm_template_deployment" "app_service_plan" {
   template_body       = "${data.template_file.deployASP.rendered}"
   name                = "${var.product}-${var.env}-asp-webapp"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  resource_group_name = "${var.resourcegroupname}"
   deployment_mode     = "Incremental"
 
   parameters = {
