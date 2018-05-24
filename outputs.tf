76e660ee3f6ad5da10f9dfe3af61a652e4392308
Ben <bhicks329@gmail.com>
2018-05-24 23:14:44 +0100

    file data as output

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "aspResourceID" {
 output "aseResourceID" {
   value = "${azurerm_template_deployment.app_service_plan.outputs["aseResourceID"]}"
 }
+
+output "ilbIP" {
+  value = "${data.local_file.ilbIP.content}"
+}
