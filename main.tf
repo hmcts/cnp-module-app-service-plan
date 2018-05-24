d2837c0a37c25ead25287799fa3c9d08f3b6841d
Ben <bhicks329@gmail.com>
2018-05-24 22:30:40 +0100

    trying to read file

--- main.tf
+++ main.tf
@@ -31,6 +31,6 @@ resource "null_resource" "ilbIP" {
   }
 
   provisioner "local-exec" {
-    command = "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-sandbox bash -e az resource show --ids ${azurerm_template_deployment.app_service_plan.outputs["aseResourceID"]}/capacities/virtualip --query internalIpAddress"
+    command = "env AZURE_CONFIG_DIR=/opt/jenkins/.azure-sandbox bash -e az resource show --ids ${azurerm_template_deployment.app_service_plan.outputs["aseResourceID"]}/capacities/virtualip --query internalIpAddress > ip.txt"
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "aspResourceID" {
 output "aseResourceID" {
   value = "${azurerm_template_deployment.app_service_plan.outputs["aseResourceID"]}"
 }
+
+output "ipbIP" {
+  value = "${file("ip.txt")}"
+}
