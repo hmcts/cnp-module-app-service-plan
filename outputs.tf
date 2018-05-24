c93ee155c4bd9e064a7fd1df719be2f631207714
Ben <bhicks329@gmail.com>
2018-05-24 21:00:40 +0100

    trying to get ILB IP

--- outputs.tf
+++ outputs.tf
@@ -5,7 +5,3 @@ output "aspResourceID" {
 output "aseResourceID" {
   value = "${azurerm_template_deployment.app_service_plan.outputs["aseResourceID"]}"
 }
-
-output "ilbIB" {
-  value = "${null_resource.ilbIP.local-exec}"
-}
