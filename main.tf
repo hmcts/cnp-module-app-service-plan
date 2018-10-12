c0793f5ff595f440d952b7842d15dca8031361bc
danielev04 <daniele@vottero.eu>
2018-10-12 16:06:15 +0100

    tags list fix

--- main.tf
+++ main.tf
@@ -45,5 +45,5 @@ resource "azurerm_app_service_plan" "app_service_plan" {
   properties{
     app_service_environment_id = "/subscriptions/${var.subscriptionId}/resourceGroups/${var.ase_name}/providers/Microsoft.Web/hostingEnvironments/${var.ase_name}"
   }
-    tags = "${var.common_tags}"
+    tags = "${jsonencode(merge(var.common_tags, var.tag_list))}"
 }
