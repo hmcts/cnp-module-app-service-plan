c6e06c68268428070120d98e29ac9c507e652aad
anand <gkdasari@gmail.com>
2019-07-30 19:10:26 +0100

    RDM-5089 - enable resource conditionally

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "aspResourceID" {
-  value = "${var.aspEnabled ? ${azurerm_template_deployment.app_service_plan.outputs["aspResourceID"]} : null}"
+  value = "${var.aspEnabled == "true" ? ${azurerm_template_deployment.app_service_plan.outputs["aspResourceID"]} : null}"
 }
