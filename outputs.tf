cd8ff601ed078d2d9e894c4cc1e44f08200e184f
anand <gkdasari@gmail.com>
2019-07-31 16:21:38 +0100

    RDM-5089 - enable resource conditionally

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
 output "aspResourceID" {
-  value = "${lookup(azurerm_template_deployment.app_service_plan.outputs, "storageAccountName", "No ASP")}"
+  value = "${ join("", azurerm_template_deployment.app_service_plan.*.outputs) }"
 }
