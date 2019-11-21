f2386031308a6d136a466a5a6e9c0f70ed277968
Piyush Kumar <piyush.m82@gmail.com>
2019-11-21 16:40:02 +0000

    remove the aspResourceID from output (#15)

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "aspResourceID" {	
-  value = "${azurerm_template_deployment.app_service_plan.outputs["aspResourceID"]}"	
-}
+# output "aspResourceID" {	
+#   value = "${azurerm_template_deployment.app_service_plan.outputs["aspResourceID"]}"	
+# }
