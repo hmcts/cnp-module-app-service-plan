773b5bdd87b33a8c5de4ebeea183644218410179
manohar.nunna <manohar.nunna@hmcts.net>
2026-03-06 16:34:48 +0000

    DTSPO-30615: modernise app service plan module
    - Replace deprecated azurerm_template_deployment + ARM JSON with native azurerm_service_plan
    - Add system-assigned managed identity
    - Rewrite variables.tf with modern HCL2 types and descriptions
    - Add outputs for asp_id, asp_name, identity principal_id and tenant_id
    - Add versions.tf with required_version >= 1.1.0 and azurerm >= 3.0.0
    - Update README.md
    - Add comment explaining why private endpoints are not on the plan

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,20 @@
-# output "aspResourceID" {	
-#   value = "${azurerm_template_deployment.app_service_plan.outputs["aspResourceID"]}"	
-# }
+output "asp_id" {
+  description = "The resource ID of the App Service Plan."
+  value       = azurerm_service_plan.app_service_plan.id
+}
+
+output "asp_name" {
+  description = "The name of the App Service Plan."
+  value       = azurerm_service_plan.app_service_plan.name
+}
+
+output "asp_identity_principal_id" {
+  description = "The principal ID of the system-assigned managed identity on the App Service Plan."
+  value       = azurerm_service_plan.app_service_plan.identity[0].principal_id
+}
+
+output "asp_identity_tenant_id" {
+  description = "The tenant ID of the system-assigned managed identity on the App Service Plan."
+  value       = azurerm_service_plan.app_service_plan.identity[0].tenant_id
+}
+
--- variables.tf
+++ variables.tf
@@ -1,59 +1,67 @@
-//SHARED VARIABLES
+# ---------------------------------------------------------------------------
+# SHARED VARIABLES
+# ---------------------------------------------------------------------------
 
 variable "location" {
-  type        = "string"
-  description = "The Azure region where you would like to deploy your app service plan to"
+  type        = string
+  description = "The Azure region where you would like to deploy your App Service Plan."
   default     = "UK South"
 }
 
 variable "resource_group_name" {
-  type        = "string"
-  description = "Resource group for asp to be deployed to"
-}
-
-//APP SERVICE PLAN SPECIFIC VARIABLES
-variable "asp_name" {
-  type        = "string"
-  description = "(Required) Specifies the plan's instance size."
-  default     = ""
-}
-
-variable "linux" {
-  type        = "string"
-  description = "(Optional) Specifies if the ASP should be of kind linux, defaults to windows."
-  default     = "false"
-}
-
-variable "asp_sku_size" {
-  type        = "string"
-  description = "(Required) Specifies the plan's instance size."
-  default     = "I1"
-}
-
-variable "asp_capacity" {
-  type        = "string"
-  description = "(Optional) Specifies the number of workers associated with this App Service Plan."
-  default     = 1
-}
-
-variable "ase_name" {
-  type        = "string"
-  description = "The name to which this ASP should be created"
+  type        = string
+  description = "Resource group for the App Service Plan to be deployed to."
 }
 
 variable "env" {
-  type        = "string"
-  description = "The deployment environment (sandbox, aat, prod etc..)"
-}
-
-variable "tag_list" {
-  type        = "map"
-  description = "List of tags to be applied to each resource"
+  type        = string
+  description = "The deployment environment (sandbox, aat, prod, etc.)."
 }
 
 variable "common_tags" {
-  type = "map"
-  default = {
-    "Team Name" = "pleaseTagMe"
-  }
+  type        = map(string)
+  description = "Common tags applied to all resources."
+  default     = {}
 }
+
+variable "tag_list" {
+  type        = map(string)
+  description = "Additional tags to be applied to each resource."
+  default     = {}
+}
+
+# ---------------------------------------------------------------------------
+# APP SERVICE PLAN VARIABLES
+# ---------------------------------------------------------------------------
+
+variable "asp_name" {
+  type        = string
+  description = "Base name of the App Service Plan. The environment suffix is appended automatically."
+}
+
+variable "linux" {
+  type        = bool
+  description = "Set to true to create a Linux App Service Plan, false for Windows."
+  default     = false
+}
+
+variable "asp_sku_size" {
+  type        = string
+  description = "The SKU name for the App Service Plan (e.g. P1v3, P2v3, I1v2, I2v2, B1, S1)."
+  default     = "P1v3"
+}
+
+variable "asp_capacity" {
+  type        = number
+  description = "The number of workers (instances) associated with this App Service Plan."
+  default     = 1
+}
+
+variable "app_service_environment_id" {
+  type        = string
+  description = "The resource ID of an App Service Environment to host the plan in. Leave empty for a multi-tenant plan."
+  default     = ""
+}
+
+
+
